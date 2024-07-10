acd0d6933bf50458e5c3e267976f2cfdf8671105
Giorgio <puncho_giorgio@inbox.lv>
2024-07-10 15:09:23 +0300

    OpenApi is added

--- support/support/urls.py
+++ support/support/urls.py
@@ -7,6 +7,26 @@ from rest_framework_simplejwt.views import TokenObtainPairView  # noqa
 from rest_framework_simplejwt.views import token_obtain_pair  # noqa
 from users.api import UserListCreateAPI, resend_activation_mail
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     # admin
     path("admin/", admin.site.urls),
@@ -23,4 +43,7 @@ urlpatterns = [
     # Authentification
     # path("auth/token/", token_obtain_pair),
     path("auth/token/", TokenObtainPairView.as_view()),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
